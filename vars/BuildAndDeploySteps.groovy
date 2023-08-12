d583c1c9082ba3c7fb311dd7925529fb673a0f07
Fathi Rida <fathiridaa@gmail.com>
2023-08-13 00:21:46 +0100

    Update BuildAndDeploySteps.groovy

--- vars/BuildAndDeploySteps.groovy
+++ vars/BuildAndDeploySteps.groovy
@@ -1,5 +1,46 @@
 package front.llc.jenkins
 
-def call() {
- print "BuildAndDeploySteps ...."
+def call(Map config) {
+    pipeline {
+        agent any
+        stages {
+            stage('Checkout') {
+                steps {
+                    print "Checkout"
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    script {
+                        Build()
+                    }
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    script {
+                       Test()
+                    }
+                }
+            }
+
+            stage('Deploy to K8S') {
+                steps {
+                    script {
+                         echo "Test"
+                    }
+                }
+            }
+
+            stage('ArgoCD Sync') {
+                steps {
+                    script {
+                        echo "ArgoCD Sync"
+                    }
+                }
+            }
+        }
+    }
 }
